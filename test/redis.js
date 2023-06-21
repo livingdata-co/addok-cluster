9556e824cb2e620e8cdb2c6ecdb0ef9d5f692b4b
Jérôme Desboeufs <jerome@livingdata.co>
2023-06-21 15:26:09 +0200

    Add support of Unix socket

--- test/redis.js
+++ test/redis.js
@@ -6,23 +6,26 @@ import {redisUrlToConfig, computeAddokRedisConfigs, createRedisRoundRobin} from 
 test('redisUrlToConfig', t => {
   t.deepEqual(redisUrlToConfig('redis://foo:12345'), {host: 'foo', port: '12345'})
   t.deepEqual(redisUrlToConfig('redis://localhost'), {host: 'localhost', port: '6379'})
+  t.deepEqual(redisUrlToConfig('unix:/run/redis.sock'), {socketPath: '/run/redis.sock'})
   t.throws(() => redisUrlToConfig('http://foo'))
 })
 
 test('computeAddokRedisConfigs / env var', t => {
-  process.env.ADDOK_REDIS_URL = 'redis://localhost:6379,redis://localhost:6380'
+  process.env.ADDOK_REDIS_URL = 'redis://localhost:6379,redis://localhost:6380,unix:/run/redis.sock'
   t.deepEqual(computeAddokRedisConfigs(), [
     {host: 'localhost', port: '6379'},
-    {host: 'localhost', port: '6380'}
+    {host: 'localhost', port: '6380'},
+    {socketPath: '/run/redis.sock'}
   ])
 })
 
 test('computeAddokRedisConfigs / param', t => {
   delete process.env.ADDOK_REDIS_URL
 
-  t.deepEqual(computeAddokRedisConfigs(['redis://foo:6379', 'redis://bar:6380']), [
+  t.deepEqual(computeAddokRedisConfigs(['redis://foo:6379', 'redis://bar:6380', 'unix:/run/redis.sock']), [
     {host: 'foo', port: '6379'},
-    {host: 'bar', port: '6380'}
+    {host: 'bar', port: '6380'},
+    {socketPath: '/run/redis.sock'}
   ])
 })
 
